eca9198f40f553ed3dcdb92c66d1a7feeaca98a4
Warrows <gaetleba@msn.com>
2012-04-26 18:48:42 -0400

    First commit

---
+++ src/com/warrows/plugins/EtherHardcore/EtherHardcore.java
@@ -0,0 +1,28 @@
+package com.warrows.plugins.EtherHardcore;
+
+import java.io.*;
+import java.util.*;
+import net.milkbowl.vault.economy.Economy;
+import net.milkbowl.vault.permission.Permission;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Loads Plugin and manages Data/Permissions
+ * 
+ * @author Warrows
+ */
+public class EtherHardcore extends JavaPlugin
+{
+	@Override
+	public void onDisable()
+	{
+	}
+	@Override
+	public void onEnable()
+	{
+	}
+}

a7a9e03cf703cb9d4d96d26812594a7c74333c27
Warrows <gaetleba@msn.com>
2012-05-09 22:11:53 -0400

    Realy workin spongial commit

--- src/com/warrows/plugins/EtherHardcore/EpongesListener.java
+++ src/com/warrows/plugins/EtherHardcore/EpongesListener.java
@@ -1,5 +1,6 @@
 package com.warrows.plugins.EtherHardcore;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,17 +13,23 @@ public class EpongesListener implements Listener
 	@EventHandler(ignoreCancelled = true)
 	public void onPlayerPickupEvent(PlayerPickupItemEvent event)
 	{
-		if (!(event.getItem().getItemStack().getType() == Material.SPONGE))
+		EtherHardcore.log.info("EtherHard: A");
+		if ((event.getItem().getItemStack().getType() != Material.SPONGE))
+		{
+			EtherHardcore.log.info("EtherHard: B");
 			return;
+		}
 
 		Player player = event.getPlayer();
 		ItemStack sponges = event.getItem().getItemStack();
-		int number = sponges.getAmount();
-
+		double number = (double) sponges.getAmount();
+		EtherHardcore.log.info("EtherHard: " + number);
 		EtherHardcore.economy.depositPlayer(player.getName(), number);
 		if (number == 1)
 			player.sendMessage("Vous avez ramassé 1 Hurbron");
 		else
 			player.sendMessage("Vous avez ramassé " + number + " Hurbrons");
+		event.getItem().teleport(new Location(EtherHardcore.world, -10, -10, -10));
+		event.setCancelled(true);
 	}
 }

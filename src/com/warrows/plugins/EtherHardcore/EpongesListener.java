43b546a898d7b7d786615525854ac4dc048da77c
Warrows <gaetleba@msn.com>
2012-05-09 20:02:10 -0400

    Fonctionnals sponges

--- src/com/warrows/plugins/EtherHardcore/EpongesListener.java
+++ src/com/warrows/plugins/EtherHardcore/EpongesListener.java
@@ -1,21 +1,28 @@
 package com.warrows.plugins.EtherHardcore;
 
-import org.bukkit.block.Block;
-import org.bukkit.entity.Enderman;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.ItemStack;
 
 public class EpongesListener implements Listener
 {
 	@EventHandler(ignoreCancelled = true)
-	public void onPlayerEvent(PlayerPickupItemEvent event)
+	public void onPlayerPickupEvent(PlayerPickupItemEvent event)
 	{
-		if (! (event.getItem() instanceof Block))
+		if (!(event.getItem().getItemStack().getType() == Material.SPONGE))
 			return;
-	
-		event.getDrops().add(new ItemStack(19,1));
+
+		Player player = event.getPlayer();
+		ItemStack sponges = event.getItem().getItemStack();
+		int number = sponges.getAmount();
+
+		EtherHardcore.economy.depositPlayer(player.getName(), number);
+		if (number == 1)
+			player.sendMessage("Vous avez ramassé 1 Hurbron");
+		else
+			player.sendMessage("Vous avez ramassé " + number + " Hurbrons");
 	}
 }
--- src/com/warrows/plugins/EtherHardcore/PlayerDeathListener.java
+++ src/com/warrows/plugins/EtherHardcore/PlayerDeathListener.java
@@ -1,7 +1,6 @@
 package com.warrows.plugins.EtherHardcore;
 
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
@@ -13,77 +12,16 @@ public class PlayerDeathListener implements Listener
 	public void onPlayerDeath(PlayerDeathEvent event)
 	{
 		Player mort = event.getEntity();
-		Player tueur = null;
-
+		int drop = 1;
+		
 		if ((mort.getLastDamageCause() instanceof Player))
 		{
-			tueur = ((Player) mort).getKiller();
-		} else
-		{
-			if ((mort.getLastDamageCause() instanceof Projectile))
-			{
-				if (((Projectile) mort).getShooter() instanceof Player)
-				{
-					tueur = (Player) ((Projectile) mort).getShooter();
-				}
-			}
+			int money = (int) EtherHardcore.economy.getBalance(mort.getName());
+			if (money>=10)
+				drop = money / 10;
 		}
 
-		/**
-		 * si la cause de la mort n'est pas un joueur, on se moque du joueur,
-		 * lui enleve un hurbron et on sors de la fonction
-		 */
-		if (tueur == null)
-		{
-			killPlayer(
-					mort,
-					"Vous êtes mort comme un faible, un Hurbron vous a quitté.",
-					1.0);
-			return;
-		}
-
-		/**
-		 * Cet int représente le différentiel de hurbron entre l'assassin et sa
-		 * victime. Si il est positif, l'assassin est plus fort. Si il est
-		 * négatif, la proie est plus forte.
-		 */
-		int diff = (int) EtherHardcore.economy.getBalance(tueur.getName())
-				- (int) EtherHardcore.economy.getBalance(mort.getName());
-
-		/**
-		 * en cas de kill à niveaux équivalents, -1/+1
-		 */
-		if (diff < -10)
-		{
-			killPlayer(mort, "Vous avez été tué. Un Hurbron vous a quitté.",
-					1.0);
-			EtherHardcore.economy.depositPlayer(tueur.getName(), 1.0);
-			tueur.sendMessage("Vous avez vaincu votre adversaire. Un de ses hurbrons vous à rejoint.");
-			// TODO algo de récup équilibré
-		} else if (diff < 5)
-		{
-			killPlayer(mort, "Vous avez été tué. Un Hurbron vous a quitté.",
-					1.0);
-			EtherHardcore.economy.depositPlayer(tueur.getName(), 1.0);
-			tueur.sendMessage("Vous avez vaincu votre adversaire. Un de ses hurbrons vous à rejoint.");
-		} else if (diff < 10)
-		{
-			killPlayer(
-					mort,
-					"Vous avez été tué par plus fort que vous, un Hurbron vous a quitté.",
-					1.0);
-			tueur.sendMessage("Vous avez vaincu plus faible que vous. Pour rien.");
-		} else
-		{
-			killPlayer(tueur,
-					"Vous avez tué un faible, un Hurbron vous a quitté.", 1.0);
-			mort.sendMessage("Votre tueur était un lache. Un de ses hurbrons l'a quitté pour sauver votre vie.");
-		}
-	}
-
-	private void killPlayer(Player mort, String msg, double penalty)
-	{
-		EtherHardcore.economy.withdrawPlayer(mort.getName(), penalty);
+		EtherHardcore.economy.withdrawPlayer(mort.getName(), drop);
 		Antilogin.addDeadPlayer(mort);
 		mort.getInventory().clear();
 		ItemStack[] noArmor = new ItemStack[4];
@@ -92,6 +30,7 @@ public class PlayerDeathListener implements Listener
 		noArmor[2] = new ItemStack(0);
 		noArmor[3] = new ItemStack(0);
 		mort.getInventory().setArmorContents(noArmor);
-		mort.kickPlayer(msg);
+		event.getDrops().add(new ItemStack(19, 1));
+		mort.kickPlayer("Vous êtes mort");
 	}
 }
